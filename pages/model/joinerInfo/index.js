3471cfd0d8e41c126feb2ce419d503a85bbf5e71
VilianLee <276550763@qq.com>
2019-09-15 10:34:44 +0800

    123

--- pages/model/joinerInfo/index.js
+++ pages/model/joinerInfo/index.js
@@ -31,10 +31,26 @@ Page({
         name: "备注",
         require: false
       }],
-      richText: [],
-      radios: [],
-      checks: [],
-      images: []
+      moreInfo: [
+        {
+          name:"测试多文本，测试多文本",
+          type: "richText"
+        },
+        {
+          name: "测试单选",
+          type: "radio",
+          options: ['1', '2']
+        },
+        {
+          name: "测试多选",
+          type: "checkbox",
+          options: ['1', '2']
+        },
+        {
+          name: "测试图片测试图片测试图片测试图片测试图片测试图片测试图片测试图片测试图片测试图片",
+          type: "picture"
+        }
+      ]
     },
   },
 
