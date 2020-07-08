a7c7822ea7d4148b8bf9524d76f11092d48e301e
VilianLee <276550763@qq.com>
2020-07-08 22:49:54 +0800

    开发环境

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const baseUrl = {
-  baseUrl: 'https://xcxtest.51bmb.com', //开发
+  // baseUrl: 'https://xcxtest.51bmb.com', //开发
+  baseUrl: 'https://xcx.51bmb.com', //生产
   imageUrl: 'http://upload.bmbee.cn',
-  webViewUrl: 'https://xcxtest.51bmb.com',
+  webViewUrl: 'https://xcx.51bmb.com',
   uploadUrl: 'upload.qiniup.com'
 };
 module.exports = { baseUrl }
+
+// "appid": "wx7758de782c51657c", 测试
+// "appid": "wx5fc3a72f2a87b0f6", 生产
